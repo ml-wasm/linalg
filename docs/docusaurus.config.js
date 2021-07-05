8b8811696e4fcc4e91ba31829bc027cefe6abc23
Archit Bhonsle <abhonsle2000@gmail.com>
2021-07-05 16:15:55 +0530

    🗃️  rudimentary docusaurus setup

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,77 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'wasml/linalg',
+  tagline: 'Linear Algebra powered by WebAssembly',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  // favicon: 'img/favicon.ico',
+  organizationName: 'wasml',
+  projectName: 'linalg',
+  themeConfig: {
+    navbar: {
+      title: 'wasml/linalg',
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
+      items: [
+        {
+          type: 'doc',
+          docId: 'getting_started',
+          position: 'left',
+          label: 'Getting Started',
+        },
+        {
+          type: 'doc',
+          docId: 'one_dimensional/index',
+          position: 'left',
+          label: 'One Dimensional',
+        },
+        {
+          type: 'doc',
+          docId: 'two_dimensional/index',
+          position: 'left',
+          label: 'Two Dimensional',
+        },
+        // { to: '/blog', label: 'Blog', position: 'left' }, Work on this later
+        {
+          href: 'https://github.com/wasml/linalg',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
