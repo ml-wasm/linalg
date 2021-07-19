dd25bd0cb305fa708fc02592384f77600aa8a19d
Archit Bhonsle <abhonsle2000@gmail.com>
2021-07-19 16:04:11 +0530

    🔧 fixed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-RUSTFLAGS='-C target-feature=+atomics,+bulk-memory' \
-	rustup run nightly-2021-02-11 \
+RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
+	rustup run nightly \
 	wasm-pack build --target web --scope wasml \
 	-- -Z build-std=panic_abort,std
