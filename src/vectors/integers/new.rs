02b69725530251d051fddbfdb25dfc9de5fd964e
Archit Bhonsle <abhonsle2000@gmail.com>
2021-08-02 19:06:23 +0530

    Revert "🤡 Int32Array for IntegersVector"
    This reverts commit cea1df371909c561399d5c708aeaa38b0e00c88c.

--- src/vectors/integers/new.rs
+++ src/vectors/integers/new.rs
@@ -54,15 +54,4 @@ impl IntegersVector {
             }),
         }
     }
-
-    /// Create a new Integers1d from Int32Array
-    #[wasm_bindgen(js_name = "newFromTypedArray")]
-    pub fn new_from_typed_array(typed_array: js_sys::Int32Array) -> IntegersVector {
-        let mut raw_data = vec![0; typed_array.length() as usize];
-        typed_array.copy_to(raw_data.as_mut_slice());
-
-        IntegersVector {
-            data: Array1::from_vec(raw_data),
-        }
-    }
 }
