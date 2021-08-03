ff675fd8436a40cbb894d3ca680aee8bcb75dcb5
Archit Bhonsle <abhonsle2000@gmail.com>
2021-08-03 15:51:36 +0530

    🚗 i32 functions

---
+++ src/constants.rs
@@ -0,0 +1,31 @@
+use wasm_bindgen::prelude::*;
+
+macro_rules! constants {
+	($($name:ident: $type:ty = $value:expr);*) => {
+		$(
+			#[wasm_bindgen]
+			#[allow(non_snake_case)]
+			pub fn $name() -> $type {
+				$value
+        	}
+		)*
+	};
+}
+
+constants!(
+    INT_MIN: i32 = i32::MIN;
+    INT_MAX: i32 = i32::MAX
+);
+
+constants!(
+    FLOAT_EPSILON: f64 = f64::EPSILON;
+    FLOAT_NAN: f64 = f64::NAN;
+    FLOAT_INFINITY: f64 = f64::INFINITY;
+    FLOAT_NEG_INFINITY: f64 = f64::NEG_INFINITY;
+    FLOAT_MAX: f64 = f64::MAX;
+    FLOAT_MIN: f64 = f64::MIN;
+    FLOAT_MIN_POSITIVE: f64 = f64::MIN_POSITIVE;
+    FLOAT_E: f64 = std::f64::consts::E;
+    FLOAT_PI: f64 = std::f64::consts::PI;
+    FLOAT_TAU: f64 = std::f64::consts::TAU
+);
--- src/matrices/integers/mod.rs
+++ src/matrices/integers/mod.rs
@@ -3,9 +3,11 @@ mod stats;
 
 use ndarray::{s, Array, Array2, Axis};
 use serde::{Deserialize, Serialize};
+use std::ops::*;
 use wasm_bindgen::prelude::*;
 
 use crate::{
+    apply_functions, apply_functions_with_arg, apply_functions_with_arg_with_alias,
     two_dimensional_basic_methods, two_dimensional_interop_methods, two_dimensional_math_methods,
     vectors::integers::IntegersVector,
 };
@@ -20,3 +22,60 @@ pub struct IntegersMatrix {
 two_dimensional_interop_methods!(IntegersMatrix, IntegersVector, i32);
 two_dimensional_basic_methods!(IntegersMatrix, IntegersVector, i32);
 two_dimensional_math_methods!(IntegersMatrix, IntegersVector, i32);
+
+apply_functions!(IntegersVector {
+    abs
+    "Computes the absolute value of each element",
+
+    saturating_abs = saturatingAbs
+    "Computes the absolute value of each element, saturating at numeric bounds instead of overflowing",
+
+    neg
+    "Negates each element",
+
+    saturating_neg = saturatingNeg
+    "Negates each element, saturating at numeric bounds instead of overflowing",
+
+    signum
+    "Changes the number to 0 if 0, -1 if negative, 1 if positive"
+});
+
+apply_functions_with_arg!(IntegersVector, u32 {
+    pow
+    "Raise each element to an integer",
+
+    saturating_pow = saturatingPow
+    "Raise each element to an integer, saturating at numeric bounds instead of overflowing"
+});
+
+apply_functions_with_arg_with_alias!(IntegersVector, i32 {
+    add | add_constant = addConstant
+    "Add an integer to each element",
+
+    sub | sub_constant = subConstant
+    "Subtract an integer from each element",
+
+    mul | mul_constant = mulConstant
+    "Multiply an integer to each element",
+
+    div | div_constant = divConstant
+    "Divide each element by an integer",
+
+    div_euclid | div_euclid_constant = divEuclidConstant
+    "Perform euclidean division on each element by an integer",
+
+    rem | rem_constant = remConstant
+    "Compute modular remainder for each element with an integer",
+
+    rem_euclid | rem_euclid_constant = remEuclidConstant
+    "Compute euclidean remainder for each element with an integer",
+
+    saturating_add | saturating_add_constant = saturatingAddConstant
+    "Add an integer to each element, saturating at numeric bounds instead of overflowing",
+
+    saturating_sub | saturating_sub_constant = saturatingSubConstant
+    "Subtract an integer from each element, saturating at numeric bounds instead of overflowing",
+
+    saturating_mul | saturating_mul_constant = saturatingMulConstant
+    "Multiply an integer to each element, saturating at numeric bounds instead of overflowing"
+});
