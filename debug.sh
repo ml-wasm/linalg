c21cf256ad1d538cf349c7c18de1014375c610a7
Archit Bhonsle <abhonsle2000@gmail.com>
2021-11-03 15:39:09 +0530

    removing a todo

---
+++ debug.sh
@@ -0,0 +1,8 @@
+cd `dirname "${BASH_SOURCE[0]}"`
+
+RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
+        rustup run nightly \
+        wasm-pack build --target web --scope ml.wasm --dev \
+        -- -Z build-std=panic_abort,std
+
+sed -i '/"files":/a \    "snippets",' pkg/package.json
