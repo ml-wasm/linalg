2060afc0500fdda1e8c2e5df2669c31cd04b91db
vedpatil611 <vedpatil611@gmail.com>
2022-02-05 19:39:36 +0530

    Build script fix

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ cd `dirname "${BASH_SOURCE[0]}"`
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
 	rustup run nightly \
 	wasm-pack build --target web --scope ml.wasm \
-	-- -Z build-std=panic_abort,std
+	-- . -Z build-std=panic_abort,std
 
 sed -i '/"files":/a \    "snippets",' pkg/package.json
